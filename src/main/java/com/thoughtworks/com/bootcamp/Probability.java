a5e9b87085bc6ba15bee9a010ef2e78505dcbed9
shreyascr <shrs601@gmail.com>
2019-09-03 23:51:03 +0530

    [shreyas] Add. nonOccurringProbability With 0.5 as Probability Occurence

--- src/main/java/com/thoughtworks/com/bootcamp/Probability.java
+++ src/main/java/com/thoughtworks/com/bootcamp/Probability.java
@@ -1,22 +1,28 @@
 package com.thoughtworks.com.bootcamp;
 
-public class Probability {
+class Probability {
     private final double probability;
 
-    public Probability(double probability) {
+    Probability(double probability) {
         this.probability = probability;
     }
 
     @Override
-    public boolean equals(Object object){
-        return (((Probability) object).probability==this.probability);
+    public boolean equals(Object object) {
+        return (((Probability) object).probability == this.probability);
     }
+
     @Override
-    public  int hashCode(){
-        if(this.probability==0.5)
-        {
-            return  1;
+    public int hashCode() {
+        if (probability == 0.5) {
+            return 1;
         }
-        return  0;
+        return 0;
     }
+
+    double nonOccuringProbability() {
+        return (1 - probability);
+    }
+
+
 }
--- src/test/java/com/thoughtworks/com/bootcamp/TestProbability.java
+++ src/test/java/com/thoughtworks/com/bootcamp/TestProbability.java
@@ -1,22 +1,26 @@
 package com.thoughtworks.com.bootcamp;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class TestProbability {
+import static org.junit.jupiter.api.Assertions.*;
 
+class TestProbability {
     @Test
     void GivenTwoProbabilityObjectWithSameZeroPointFive_WhenEqualsCalled_ThenReturnTrue() {
         Probability probabilityWithZeroPointFive = new Probability(0.5);
         Probability anotherProbbilityWithZeroPointFive = new Probability(0.5);
-        Assertions.assertTrue(probabilityWithZeroPointFive.equals(anotherProbbilityWithZeroPointFive));
+        assertTrue(probabilityWithZeroPointFive.equals(anotherProbbilityWithZeroPointFive));
     }
 
-    void GivenTwoProbabilityObjectWithSameZeroPointFive_WhenHashCodeCalled_ThenReturnTrue() {
+    void GivenTwoProbabilityObjectWithSameZeroPointFive_WhenHashCodeCalled_ThenExpectedIsOne() {
         Probability probabilityWithZeroPointFive = new Probability(0.5);
-        Probability anotherProbbilityWithZeroPointFive = new Probability(0.5);
-        Assertions.assertEquals(1,probabilityWithZeroPointFive.hashCode());
+        assertEquals(1, probabilityWithZeroPointFive.hashCode());
+    }
+    @Test
+    void givenProbability_WhenNonProbabilityCalled_ThenReturnZeroPointFive(){
+        Probability probabilityWithZeroPointFive = new Probability(0.5);
+        assertEquals(1-0.5,probabilityWithZeroPointFive.nonOccuringProbability());
     }
 
 }
